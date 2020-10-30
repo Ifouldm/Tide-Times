217602174b84ba2c11f8167de426b353004a8543
Matt I <Ifouldm@users.noreply.github.com>
2020-10-30 14:24:16 +0000

    Delete constants.dart

--- lib/utils/constants.dart
+++
@@ -1,7 +0,0 @@
-String kAPIKey = 'API KEY GOES HERE';
-String kURL = 'https://tides.p.rapidapi.com';
-double lat = 0;
-double lon = 0;
-int duration = 4320;
-String kOptions =
-    '/tides?interval=60&duration=$duration&latitude=$lat&longitude=$lon';
