5fe1d683aae801ec976773d744f3281c6671136b
Matt I <Ifouldm@users.noreply.github.com>
2020-10-30 14:24:37 +0000

    Create constants.dart

---
+++ lib/utils/constants.dart
@@ -0,0 +1,8 @@
+
+String kAPIKey = 'API KEY GOES HERE';
+String kURL = 'https://tides.p.rapidapi.com';
+double lat = 0;
+double lon = 0;
+int duration = 4320;
+String kOptions =
+    '/tides?interval=60&duration=$duration&latitude=$lat&longitude=$lon';
