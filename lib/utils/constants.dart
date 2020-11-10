9e04f678a1dd464169618f58e046919e2b1f2bfc
matt <matt@linux>
2020-11-10 14:30:23 +0000

    new constants.dart

---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+String kURL = 'https://tides.p.rapidapi.com';
+double lat = 50.78;
+double lon = -0.69;
+int duration = 4320;
+String kOptions =
+    '/tides?interval=60&duration=$duration&latitude=$lat&longitude=$lon';
